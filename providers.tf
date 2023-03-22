da6d2c231c092c42ca775911e592e77d0c4c02ec
blahsi <113141616+blahsi@users.noreply.github.com>
2023-03-22 15:25:08 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,6 +1,11 @@
 terraform {
   required_version = ">=1.0"
-
+  backend "azurerm" {
+    resource_group_name  = "kopicloud-tfstate-rg"
+    storage_account_name = "kopicloudiactest"
+    container_name       = "core-tfstate"
+    key                  = "actions.tfstate"
+    
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
