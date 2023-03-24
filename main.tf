11a72619b41884353a2b7648d217bfb676fa94da
blahsi <113141616+blahsi@users.noreply.github.com>
2023-03-24 10:20:58 -0500

    drift test 2

--- main.tf
+++ main.tf
@@ -38,6 +38,15 @@ resource "azurerm_subnet" "my_terraform_subnet" {
   virtual_network_name = azurerm_virtual_network.my_terraform_network.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+}
+
+# Create subnet
+resource "azurerm_subnet" "drift_subnet" {
+  name                 = "${random_pet.prefix.id}-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.my_terraform_network.name
+  address_prefixes     = ["10.0.0.0/24"]
+}
 
 # Create public IPs
 resource "azurerm_public_ip" "my_terraform_public_ip" {
@@ -67,7 +76,7 @@ resource "azurerm_network_security_group" "my_terraform_nsg" {
     name                       = "RDP"
     priority                   = 1000
     direction                  = "Inbound"
-    access                     = "Allow"
+    access                     = "Deny"
     protocol                   = "*"
     source_port_range          = "*"
     destination_port_range     = "3389"
@@ -78,7 +87,7 @@ resource "azurerm_network_security_group" "my_terraform_nsg" {
     name                       = "web"
     priority                   = 1001
     direction                  = "Inbound"
-    access                     = "Allow"
+    access                     = "Deny"
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "80"
